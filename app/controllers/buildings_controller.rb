0a5f56d9d663f3215ed3101e67740d545b6e536c
casperjureskog <lcasper@mac.com>
2017-08-14 10:21:03 +0200

    adds session on bulding creation

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -5,8 +5,11 @@ class BuildingsController < ApplicationController
 
   def create
     @building = Building.new building_params
+    @building.update(user_id: current_user.id)
     if @building.save
       flash[:notice] = "New house created!"
+      @building = Building.find_by(user_id: current_user)
+      session[:current_building_id] = @building.id
       redirect_back(fallback_location: root_path)
     end
   end
