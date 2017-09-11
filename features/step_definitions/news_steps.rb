e4d9d0589ad271d34b476281680173aece345dc4
johan <johan.schantz@telia.com>
2017-09-11 12:02:49 +0200

    add building id to feature test

--- features/step_definitions/news_steps.rb
+++ features/step_definitions/news_steps.rb
@@ -1,5 +1,6 @@
 Given(/^the following news exist$/) do |table|
+  building = Building.find_by(name: "bighouse")
   table.hashes.each do |hash|
-    create(:news, title: hash[:title], description: hash[:description])
+    create(:news, title: hash[:title], description: hash[:description], building_id: building.id)
   end
 end
